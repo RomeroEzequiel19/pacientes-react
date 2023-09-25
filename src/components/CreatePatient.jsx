10fc9b289bf6438ae4658b6348b1453c2fd0b69c
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-09-25 14:04:33 -0300

    feat: Se agregó los componentes para el formulario de alta de los pacientes

---
+++ src/components/CreatePatient.jsx
@@ -0,0 +1,19 @@
+import { Header } from "./Header"
+import { FormCreate } from "./FormCreate"
+import { Footer } from "./Footer"
+
+function CreatePatient() {
+
+
+  return(
+   <>
+   <Header />
+   <FormCreate />
+   <Footer />
+   </>
+    
+  )
+  
+}
+
+export default CreatePatient
